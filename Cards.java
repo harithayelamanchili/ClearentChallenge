02ae35074a28572b7531dfa9895327666cfc81fd
Haritha Yelamanchili <haritha.manchili@gmail.com>
2016-07-23 18:39:23 -0500

    Add files via upload

---
+++ Cards.java
@@ -0,0 +1,17 @@
+package practice1;
+
+public class Cards {
+	public static double visa(double v){
+		double vInterest = v*10/100;
+		return vInterest;
+	}
+	public static double MC(double mc){
+		double MCInterest = mc*5/100;
+		return MCInterest;
+	}
+	public static double Discover(double d){
+		double dInterest = d*1/100;
+		return dInterest;
+	}
+}
+	
